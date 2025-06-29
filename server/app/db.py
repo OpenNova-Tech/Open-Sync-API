739e6feddb8ee7472581f6bc4a4ad62fb4f6e656
Tijo <bhattacharjeeanurag3@gmail.com>
2025-06-30 01:56:32 +0530

    mongodb connection

---
+++ server/app/db.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+from motor.motor_asyncio import AsyncIOMotorClient
+
+load_dotenv()
+MONGO_URL = os.getenv("MONGO_URL")
+client = AsyncIOMotorClient(MONGO_URL)
+db = client["Open-Sync"]
